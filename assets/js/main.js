3ed5cc01cdc58a3c5d7f61140166682efaa3e088
davidMedhatM <david.medhat.m@gmail.com>
2024-07-14 21:19:16 +0300

    sec commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@ function displayCategories(arr) {
                     <img class="w-100" src="${arr[i].strCategoryThumb}" alt="" srcset="">
                     <div class="meal-layer position-absolute text-center text-black p-2">
                         <h3>${arr[i].strCategory}</h3>
-                        <p>${arr[i].strCategoryDescription.split(" ").slice(0,20).join(" ")}</p>
+                        <p>${arr[i].strCategoryDescription.split(" ").slice(0, 20).join(" ")}</p>
                     </div>
                 </div>
         </div>
@@ -141,7 +141,7 @@ function displayIngredients(arr) {
                 <div onclick="getIngredientsMeals('${arr[i].strIngredient}')" class="rounded-2 text-center cursor-pointer">
                         <i class="fa-solid fa-drumstick-bite fa-4x"></i>
                         <h3>${arr[i].strIngredient}</h3>
-                        <p>${arr[i].strDescription.split(" ").slice(0,20).join(" ")}</p>
+                        <p>${arr[i].strDescription.split(" ").slice(0, 20).join(" ")}</p>
                 </div>
         </div>
         `
@@ -210,7 +210,7 @@ async function getMealDetails(mealID) {
 
 
 function displayMealDetails(meal) {
-    
+
     searchContainer.innerHTML = "";
 
 
@@ -317,4 +317,179 @@ function displayMeals(arr) {
     }
 
     rowData.innerHTML = cartoona
+}
+
+
+function showContacts() {
+    rowData.innerHTML = `<div class="contact min-vh-100 d-flex justify-content-center align-items-center">
+    <div class="container w-75 text-center">
+        <div class="row g-4">
+            <div class="col-md-6">
+                <input id="nameInput" onkeyup="inputsValidation()" type="text" class="form-control" placeholder="Enter Your Name">
+                <div id="nameAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Special characters and numbers not allowed
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="emailInput" onkeyup="inputsValidation()" type="email" class="form-control " placeholder="Enter Your Email">
+                <div id="emailAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Email not valid *exemple@yyy.zzz
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="phoneInput" onkeyup="inputsValidation()" type="text" class="form-control " placeholder="Enter Your Phone">
+                <div id="phoneAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid Phone Number
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input id="ageInput" onkeyup="inputsValidation()" type="number" class="form-control " placeholder="Enter Your Age">
+                <div id="ageAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid age
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input  id="passwordInput" onkeyup="inputsValidation()" type="password" class="form-control " placeholder="Enter Your Password">
+                <div id="passwordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid password *Minimum eight characters, at least one letter and one number:*
+                </div>
+            </div>
+            <div class="col-md-6">
+                <input  id="repasswordInput" onkeyup="inputsValidation()" type="password" class="form-control " placeholder="Repassword">
+                <div id="repasswordAlert" class="alert alert-danger w-100 mt-2 d-none">
+                    Enter valid repassword 
+                </div>
+            </div>
+        </div>
+        <button id="submitBtn" disabled class="btn btn-outline-danger px-2 mt-3">Submit</button>
+    </div>
+</div> `
+    submitBtn = $("#submitBtn")
+
+    $("#nameInput").focus( _ => {
+        nameInputTouched = true
+    })
+
+    $("#emailInput").focus( _ => {
+        emailInputTouched = true
+    })
+
+    $("#phoneInput").focus( _ => {
+        phoneInputTouched = true
+    })
+
+    $("#ageInput").focus( _ => {
+        ageInputTouched = true
+    })
+
+    $("#passwordInput").focus( _ => {
+        passwordInputTouched = true
+    })
+
+    $("#repasswordInput").focus( _ => {
+        repasswordInputTouched = true
+    })
+
+}
+
+let nameInputTouched = false;
+let emailInputTouched = false;
+let phoneInputTouched = false;
+let ageInputTouched = false;
+let passwordInputTouched = false;
+let repasswordInputTouched = false;
+
+
+
+
+function inputsValidation() {
+    if (nameInputTouched) {
+        if (nameValidation()) {
+            document.getElementById("nameAlert").classList.replace("d-block", "d-none")
+
+        } else {
+            document.getElementById("nameAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (emailInputTouched) {
+
+        if (emailValidation()) {
+            document.getElementById("emailAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("emailAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (phoneInputTouched) {
+        if (phoneValidation()) {
+            document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (ageInputTouched) {
+        if (ageValidation()) {
+            document.getElementById("ageAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("ageAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (passwordInputTouched) {
+        if (passwordValidation()) {
+            document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (repasswordInputTouched) {
+        if (repasswordValidation()) {
+            document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+
+    if (nameValidation() &&
+        emailValidation() &&
+        phoneValidation() &&
+        ageValidation() &&
+        passwordValidation() &&
+        repasswordValidation()) {
+        submitBtn.removeAttribute("disabled")
+    } else {
+        submitBtn.setAttribute("disabled", true)
+    }
+}
+
+function nameValidation() {
+    return (/^[a-zA-Z ]+$/.test(document.getElementById("nameInput").value))
+}
+
+function emailValidation() {
+    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("emailInput").value))
+}
+
+function phoneValidation() {
+    return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("phoneInput").value))
+}
+
+function ageValidation() {
+    return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("ageInput").value))
+}
+
+function passwordValidation() {
+    return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("passwordInput").value))
+}
+
+function repasswordValidation() {
+    return document.getElementById("repasswordInput").value == document.getElementById("passwordInput").value
 }
